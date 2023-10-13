136dc6584bf1cb41144ff7a3406d55d4524d5073
dishan <dishankothiya@gmail.com>
2023-10-13 17:24:14 +0530

    devicepreviwe

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DevicePreview(builder: (context) => MyApp()));
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "codemagic",
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: Home(),
     );
   }
 }
-
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -28,7 +34,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hello"),
+        child: Text("hello dishan"),
       ),
     );
   }
